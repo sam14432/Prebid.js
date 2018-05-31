1b779a6d9bfea8de61590d60e945e789d8cf361b
SamuelPalmer <samuel.palmer@relevant-digital.com>
2018-06-01 00:19:44 +0200

    fix

--- modules/relevant-client.js
+++ modules/relevant-client.js
@@ -1,33 +1,150 @@
 /* eslint-disable */
 import * as utils from 'src/utils';
 
+const setSize = (elm, width, height) => {
+  const toDim = v => isNaN(v) ? v : v + "px";
+  if (width != null) {
+    elm.style.width = toDim(width);
+  }
+  if (height != null) {
+    elm.style.height = toDim(width);
+  }
+};
+
+const PASSBACK_HTML = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset='UTF-8'/>
+  </head>
+  <body style='margin:0px; border:0px; padding:0px;'>
+    <script>
+        postbid();
+    </script>
+  </body>
+  </html>      
+`;
+
 class PostbidAuction
 {
   constructor(worker, params) {
     // other values: bids, sizes, legacyPassbackHtml, IframeResizerMinHeight
+    // location { win, insertAfter, insertIn }
     const DEFAULT = {
       bidTimeOut: 1000,
-      useIframeResizer: true,
-      enableConsoleLogs: true,
-      enableIframeResizerLogging: false,
       logIdentifier: 'unknown',
+      containers: [],
     };
     Object.assign(this, DEFAULT, params, {
       worker,
       pbjs: worker.pbjs,
+      unitId: `unit_${Math.random().toString().substring(2)}`,
     });
+    if(Array.isArray(this.sizes) && Array.isArray(this.sizes[0])) {
+      this.initWidth = this.sizes[0][0];
+      this.initHeight = this.sizes[0][1];
+    }
+    if(!this.initWidth || !this.initHeight) {
+      throw Error('sizes invalid');
+    }
+  }
+
+  log(str) {
+    utils.logInfo(`Postbid: ${this.logIdentifier} - ${str}`);
+  }
+
+  resize(width, height) {
+    if (!this.hasResized) {
+      (this.containers || []).forEach(c => setSize(c, 'auto', 'auto'));
+      this.hasResized = true;
+    }
+    try {
+      if (this.win.frameElement) {
+        setSize(this.win.frameElement, width, height)
+      }
+    } catch(e) { /** In un-friendly iframe */ }
+    setSize(this.iframe, width, height);
   }
 
   initIframe() {
-    //if()
+    let { win, insertAfter, appendTo } = this.location || {};
+    if (!win) {
+      throw Error('location.win not defined');
+    }
+    const noSpec = !insertAfter && !appendTo;
+    const doc = win.document;
+    if (win === top) {
+      if(noSpec) {
+        throw Error('Need to specify where to insert iframe if in top window');
+      }
+    }
+    if(noSpec) {
+      if (!doc.body) {
+        throw Error(`document.body missing`);
+      }
+      appendTo = doc.body;
+    }
+
+    var iframe = doc.createElement('iframe');
+    const attribs = {
+      FRAMEBORDER: 0,
+      SCROLLING: 'no',
+      MARGINHEIGHT: 0,
+      MARGINWIDTH: 0,
+      TOPMARGIN: 0,
+      LEFTMARGIN: 0,
+      ALLOWTRANSPARENCY: 'true',
+      width: this.initWidth,
+      height: this.initHeight,
+    };
+    for (const [key, value] of Object.entries(attribs)) {
+      iframe.setAttribute(key, value);
+    }
+    if (insertAfter) {
+      insertAfter.parentNode.insertBefore(iframe, insertAfter.nextSibling);
+    } else if (appendTo) {
+      appendTo.appendChild(iframe);
+    }
+    this.iframe = iframe;
   }
 
   run() {
-    if(this.useIframeResizer) {
-      //const raw = require('!raw-loader!iframe-resizer/js/iframeResizer.contentWindow');
-      require('iframe-resizer/js/iframeResizer'); // will only run once
-    }
     this.initIframe();
+    pbjs.addAdUnits([{
+      code: this.unitId,
+      sizes: this.sizes,
+      bids: this.bids,
+    }]);
+    pbjs.requestBids({
+      adUnitCodes: [this.unitId],
+      timeout: this.bidTimeOut,
+      bidsBackHandler: () => this.onBidsBack(),
+    });
+  }
+
+  onBidsBack() {
+    var params = this.pbjs.getAdserverTargetingForAdUnitCode(this.unitId);
+    if (params && params.hb_adid) {
+      this.log(`Bid won - rendering ad: ${params}`);
+      const dimensions = (params.hb_size || '').split('x');
+      if(dimensions.length === 2) {
+        this.setSize(dimensions[0], dimensions[1]);
+      }
+      this.pbjs.renderAd(iframeDoc, params.hb_adid);
+    } else {
+      this.log('Calling passback');
+      const ifrDoc = this.iframe.contentWindow.document;
+      ifrDoc.open();
+      this.iframe.contentWindow.postbid = () => {
+        let { passbackHtml } = this;
+        if (!passbackHtml && this.legacyPassbackHtml) {
+          passbackHtml = eval("'" + this.legacyPassbackHtml + "'");
+        }
+        ifrDoc.write(passbackHtml || '');
+      };
+      ifrDoc.write(PASSBACK_HTML);
+      ifrDoc.close();
+    }
   }
 }
 
@@ -39,7 +156,10 @@ class RelevantWorker
   }
 
   init() {
-    this.pbjs.setConfig({ consentManagement: {} });
+    this.pbjs.setConfig({
+      consentManagement: {},
+      debug: location.toString().indexOf('relevant-debug'),
+    });
     this.queue.forEach(param => this.runCmd(param));
   }
 
